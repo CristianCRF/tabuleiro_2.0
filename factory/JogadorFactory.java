8e37b4cd69d0c9b5a172dd98d7006d45508faf17
Capim32 <victorsbn3@gmail.com>
2025-12-06 22:40:00 -0300

    mudanças no factory e entidade

--- factory/JogadorFactory.java
+++ factory/JogadorFactory.java
@@ -1,5 +1,9 @@
 package factory;
 
-public class JogadorFactory {
+import jogadores.*;
 
+public class JogadorFactory {
+    public static Jogador criarJogador(int tipo, int codigoCor) {
+        
+    }
 }
